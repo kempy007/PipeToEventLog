29ba1dd76eaf351bb30455486f0f1a8817dd6574
Martyn Kemp <martyn.kemp@midlandhr.co.uk>
2017-06-09 14:32:34 +0100

    First Commit

---
+++ PipeToEventLog/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipeToEventLog
+{
+    class Program
+    {
+        static bool running = true;
+        static bool verbosedebug = false;
+        static int counter = 0;
+        static int Main(string[] args)
+        {
+
+            string input = "";
+            while (running) // OUTERLOOP
+            {
+                if (!IsPipedInput())
+                    return 0;
+                Console.SetIn(new StreamReader(Console.OpenStandardInput(8192))); // This will allow input >256 chars
+                while (Console.In.Peek() != -1) // DATALOOP
+                {
+                    input = Console.In.ReadLine();
+                    myWriteEvent(input);
+                    Debug.WriteLine("Data read was " + input);
+                    counter = 0; // reset counter 
+                    input = "";
+                }
+            }
+            return 0;
+        }
+
+        private static void myWriteEvent(string strz)
+        {
+            EventLog myLog = new EventLog();
+            myLog.Log = "JUNIPER-FW";
+            myLog.Source = "JUNIPER-FW";
+            myLog.WriteEntry(strz, EventLogEntryType.Information, 96);
+            myLog.Close();
+            //myLog.Dispose(); // Analysis by ms tools said this was bad!
+        }
+
+        private static bool IsPipedInput()
+        {
+            try
+            {
+                bool redirected = Console.IsInputRedirected; // is always true
+                int peekVal = Console.In.Peek();
+                if (redirected == false) { Debug.WriteLine("Redirected was false"); }
+                if (peekVal != -1)
+                {
+                    Debug.WriteLine("Has Data"); // only hit on startup
+                }
+                else
+                {
+                    Debug.WriteLine("No Data"); // mostly hit but the data loop keeps count low like LT 1
+                    counter++;
+                }
+                if(counter >= 100) { Debug.WriteLine("Counter hit >= 100"); running = false; }
+                bool isKey = Console.KeyAvailable; // intentionally throws an error
+                return false;
+            }
+            catch(Exception e)
+            {   
+                if(verbosedebug) { Debug.WriteLine(e.Message); }
+                return true;
+            }
+        }
+    }
+}
---
+++ ReadInWriteOut/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadInWriteOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is just a test app, pipe it to real app and watch debug when you type input and press enter.
+            //  callthis.exe | pipetoeventlog.exe 
+            string s;
+            while ((s = Console.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
